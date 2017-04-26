ecfae65bf04d2a44f8cf892e32ea13b3a451e061
YZY_PRO <976809588@qq.com>
2017-04-26 14:44:44 +0800

    Initial Commit

---
+++ YZYPhotoPicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YZYPhotoPicker
+//
+//  Created by YZY on 2017/4/26.
+//  Copyright © 2017年 YZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YZYPhotoPicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YZYPhotoPicker
+//
+//  Created by YZY on 2017/4/26.
+//  Copyright © 2017年 YZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
